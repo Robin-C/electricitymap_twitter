efcab2bc2b12e428d1494806eb5f949b90da1ee8
Robin C <r.chesne@gmail.com>
2022-02-15 17:09:58 +0100

    top3 sources done

--- scripts/get_data/main.py
+++ scripts/get_data/main.py
@@ -1,4 +1,4 @@
-import requests, os, psycopg2, psycopg2.extras, random, itertools
+import requests, os, psycopg2, psycopg2.extras, random, itertools, collections
 
 electricitymap_token = os.getenv('AUTH_ELECTRICITYMAP')
 
@@ -38,12 +38,15 @@ def get_matchup():
     zones = list(itertools.chain.from_iterable(zones))
     zone_ids.append(zones)
 
-def get_co2(country):
+def get_zones(country):
    # First we get the zones for each country
    query = cur.execute("select zone_id from public.zone where country = %s ", (country,))
    zones_country = cur.fetchall()
    zones_country = list(itertools.chain.from_iterable(zones_country))
+   return zones_country
 
+def get_co2(country):
+   zones_country = get_zones(country)
    country_co2 = {'country': country}
    country_co2_levels_raw = []
    country_co2_levels_cleaned = []
@@ -61,13 +64,72 @@ def get_co2(country):
    co2 = round(sum(country_co2_levels_cleaned) / sum(country_production_levels))
    return co2
 
+def get_mix(country):
+  zones_country = get_zones(country)
+  mix = {
+  'nuclear': 0,
+  'geothermal': 0,
+  'biomass': 0,
+  'coal': 0,
+  'wind': 0,
+  'solar': 0,
+  'hydro': 0,
+  'gas': 0,
+  'oil': 0,
+  'unknown': 0,
+  'hydro_discharge': 0,
+  'battery_discharge': 0,
+  'total_production': 0
+  }
+
+  for zone in zones_country:
+    res = requests.get(f"https://api.electricitymap.org/v3/power-breakdown/latest?zone={zone}", headers={'auth-token':electricitymap_token}).json()
+    production = res.get('powerProductionBreakdown')
+    mix['total_production'] += res.get('powerProductionTotal')
+
+   # We get mix details
+    for keys in production:
+        if keys == 'nuclear':
+          mix['nuclear'] += production.get(keys) if production.get(keys) is not None else 0
+        if keys == 'geothermal':
+          mix['geothermal'] += production.get(keys) if production.get(keys) is not None else 0
+        if keys == 'biomass':
+          mix['biomass'] += production.get(keys) if production.get(keys) is not None else 0
+        if keys == 'coal':
+          mix['coal'] += production.get(keys) if production.get(keys) is not None else 0
+        if keys == 'wind':
+          mix['wind'] += production.get(keys) if production.get(keys) is not None else 0
+        if keys == 'solar':
+          mix['solar'] += production.get(keys) if production.get(keys) is not None else 0                           
+        if keys == 'hydro':
+          mix['hydro'] += production.get(keys) if production.get(keys) is not None else 0 
+        if keys == 'gas':
+          mix['gas'] += production.get(keys) if production.get(keys) is not None else 0
+        if keys == 'oil':
+          mix['oil'] += production.get(keys) if production.get(keys) is not None else 0 
+        if keys == 'unknown':
+          mix['unknown'] += production.get(keys) if production.get(keys) is not None else 0                                                                            
+        if keys == 'hydro discharge':
+          mix['hydro_discharge'] += production.get(keys) if production.get(keys) is not None else 0
+        if keys == 'battery discharge':
+          mix['battery_discharge'] += production.get(keys) if production.get(keys) is not None else 0    
+
+  # We get the top 3 energy source for the country
+  tups = collections.Counter(mix).most_common(4)
+  top3 = []
+  for tuple in tups:
+    if tuple[0] != 'total_production':
+      top3.append(tuple + (str(round(tuple[1] / mix['total_production']*100, 2)) + '%',))
+  print(top3)
 
 def get_data(matchup, country1, country2):
   get_co2(country1)
   get_co2(country2)
+  get_mix(country2)
+
 
 
 
 
 # get_matchup()
-get_data('central', 'Danemark', 'France')
+print(get_data('central', 'Danemark', 'France'))
--- scripts/get_data/test.py
+++ scripts/get_data/test.py
@@ -2,18 +2,6 @@ import requests, os, psycopg2, psycopg2.extras, random, itertools
 
 electricitymap_token = os.getenv('AUTH_ELECTRICITYMAP')
 
-# postgres set up
-host = 'postgres'
-dbname = os.getenv('POSTGRES_DB')
-user = os.getenv('POSTGRES_USER')
-password = os.getenv('POSTGRES_PASSWORD')
-conn = psycopg2.connect(f"host={host} dbname={dbname} user={user} password={password}")
-cur = conn.cursor()
-cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+res = requests.get(f"https://api.electricitymap.org/v3/power-breakdown/latest?zone=DK-DK1", headers={'auth-token':electricitymap_token}).json()
 
-qry = cur.execute("select zone_id from public.zone where country = 'Danemark'")
-res = cur.fetchall()
-
-res_unnested = list(itertools.chain.from_iterable(res))
-
-print(res_unnested)
+print(res)
