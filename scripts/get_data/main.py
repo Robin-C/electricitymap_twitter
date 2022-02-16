33d5e96f4122c0990a170ec84d14d9163df49bc1
Robin C <r.chesne@gmail.com>
2022-02-16 12:19:33 +0100

    store data to postgres

--- scripts/get_data/main.py
+++ scripts/get_data/main.py
@@ -1,4 +1,4 @@
-import requests, os, psycopg2, psycopg2.extras, random, itertools, collections
+import requests, os, psycopg2, psycopg2.extras, random, itertools, collections, datetime
 
 electricitymap_token = os.getenv('AUTH_ELECTRICITYMAP')
 
@@ -20,23 +20,25 @@ cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 matchup = []
 zone_ids = []
 
-def get_matchup():
+def get_matchups(divisions = 'all'):
   # We query the divisions
-  query = cur.execute('select division from public.zone group by 1')
-  divisions = cur.fetchall()
+  if divisions == 'all':
+    query = cur.execute('select division from public.zone group by 1')
+    divisions = cur.fetchall()
+
   for division in divisions:
-    query = cur.execute('select country from public.zone where division = %s group by 1', (division[0],))
+    division = division[0]
+    cur.execute('select country from public.zone where division = %s group by 1', (division,))
     countries = cur.fetchall()
     countries = list(itertools.chain.from_iterable(countries))
+    print(division)
+    print(countries)
+    print(len(countries))
     runners = len(countries)
     winners = random.sample(range(0, runners), 2)
-    # Finally we get the zone_id for each country
-    print(countries)
-    query = cur.execute("select zone_id from public.zone where country = %s or country = %s", (countries[winners[0]],countries[winners[1]]))
-    zones = cur.fetchall()
-    # Unnest the query result
-    zones = list(itertools.chain.from_iterable(zones))
-    zone_ids.append(zones)
+    country1 = countries[winners[0]]
+    country2 = countries[winners[1]]
+    get_data(division, country1, country2)
 
 def get_zones(country):
    # First we get the zones for each country
@@ -45,9 +47,8 @@ def get_zones(country):
    zones_country = list(itertools.chain.from_iterable(zones_country))
    return zones_country
 
-def get_co2(country):
+def get_co2(country, division):
    zones_country = get_zones(country)
-   country_co2 = {'country': country}
    country_co2_levels_raw = []
    country_co2_levels_cleaned = []
    country_production_levels = []
@@ -55,7 +56,13 @@ def get_co2(country):
    for zone in zones_country:
      co2 = requests.get(f"https://api.electricitymap.org/v3/carbon-intensity/latest?zone={zone}", headers={'auth-token':electricitymap_token}).json()
      country_co2_levels_raw.append(co2.get('carbonIntensity'))
+     if country_co2_levels_raw == None:
+       get_matchups(division)
+       return False
      production = requests.get(f"https://api.electricitymap.org/v3/power-breakdown/latest?zone={zone}", headers={'auth-token':electricitymap_token}).json()
+     if production.get('error'):
+       get_matchups(division)
+       return False
      country_production_levels.append(production.get('powerProductionTotal'))
 
    for co2_level, production_level in zip(country_co2_levels_raw, country_production_levels):
@@ -64,7 +71,7 @@ def get_co2(country):
    co2 = round(sum(country_co2_levels_cleaned) / sum(country_production_levels))
    return co2
 
-def get_mix(country):
+def get_mix(country, division):
   zones_country = get_zones(country)
   mix = {
   'nuclear': 0,
@@ -85,6 +92,9 @@ def get_mix(country):
   for zone in zones_country:
     res = requests.get(f"https://api.electricitymap.org/v3/power-breakdown/latest?zone={zone}", headers={'auth-token':electricitymap_token}).json()
     production = res.get('powerProductionBreakdown')
+    if production == None:
+      get_matchups(division)
+      return False
     mix['total_production'] += res.get('powerProductionTotal')
 
    # We get mix details
@@ -119,17 +129,48 @@ def get_mix(country):
   top3 = []
   for tuple in tups:
     if tuple[0] != 'total_production':
-      top3.append(tuple + (str(round(tuple[1] / mix['total_production']*100, 2)) + '%',))
-  print(top3)
+      top3.append(tuple + (str(round(tuple[1] / mix['total_production']*100)),))
 
-def get_data(matchup, country1, country2):
-  get_co2(country1)
-  get_co2(country2)
-  get_mix(country2)
+  return top3
+
+def get_data(division, country1, country2):
+  co2_country1 = get_co2(country1, division)
+  co2_country2 = get_co2(country2, division)
+  mix_country1 = get_mix(country1, division)
+  mix_country2 = get_mix(country2, division)
+  if co2_country1 and co2_country2 and mix_country1 and mix_country2:
+    print(f'{country1}: CO2 {co2_country1}, mix {mix_country1}')
+    print(f'{country2}: CO2 {co2_country2}, mix {mix_country2}')
+    cur.execute("select coalesce(max(match_id), 0) from data_fetched")
+    match_id = cur.fetchone()[0] + 1
+    created_at_utc = datetime.datetime.now(datetime.timezone.utc)
+    # We store the result to postgres
+    cur.execute("insert into data_fetched (match_id, division, country, co2, mix_1, mix_2, mix_3 \
+               , created_at_utc, has_been_tweeted, tweeted_at_utc, mix_1_mwh, mix_2_mwh, mix_3_mwh \
+               , mix_1_pct, mix_2_pct, mix_3_pct) \
+                VALUES(%s, %s, %s, %s, %s, %s, %s, \
+                       %s, %s, %s, %s, %s, %s, \
+                       %s, %s, %s)", \
+                ( \
+                  match_id, division, country1, co2_country1, mix_country1[0][0], mix_country1[1][0], mix_country1[2][0], \
+                 created_at_utc, False, None,  mix_country1[0][1], mix_country1[1][1], mix_country1[2][1], \
+                 mix_country1[0][2], mix_country1[1][2], mix_country1[2][2]
+                )
+               )
+    cur.execute("insert into data_fetched (match_id, division, country, co2, mix_1, mix_2, mix_3 \
+            , created_at_utc, has_been_tweeted, tweeted_at_utc, mix_1_mwh, mix_2_mwh, mix_3_mwh \
+            , mix_1_pct, mix_2_pct, mix_3_pct) \
+            VALUES(%s, %s, %s, %s, %s, %s, %s, \
+                    %s, %s, %s, %s, %s, %s, \
+                    %s, %s, %s)", \
+            ( \
+              match_id, division, country2, co2_country2, mix_country2[0][0], mix_country2[1][0], mix_country2[2][0], \
+              created_at_utc, False, None,  mix_country2[0][1], mix_country2[1][1], mix_country2[2][1], \
+              mix_country2[0][2], mix_country2[1][2], mix_country2[2][2]
+            )
+            )
+    conn.commit()
+    # We generate tweet
 
 
-
-
-
-# get_matchup()
-print(get_data('central', 'Danemark', 'France'))
+get_matchups()
--- scripts/get_data/test.py
+++ scripts/get_data/test.py
@@ -1,7 +1,25 @@
+from asyncio.windows_events import NULL
 import requests, os, psycopg2, psycopg2.extras, random, itertools
 
-electricitymap_token = os.getenv('AUTH_ELECTRICITYMAP')
+# postgres set up
+host = 'postgres'
+dbname = os.getenv('POSTGRES_DB')
+user = os.getenv('POSTGRES_USER')
+password = os.getenv('POSTGRES_PASSWORD')
+conn = psycopg2.connect(f"host={host} dbname={dbname} user={user} password={password}")
+cur = conn.cursor()
+cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
-res = requests.get(f"https://api.electricitymap.org/v3/power-breakdown/latest?zone=DK-DK1", headers={'auth-token':electricitymap_token}).json()
-
-print(res)
+cur.execute("create table if not exists data_fetched (\
+ ID  int NOT NULL AUTO_INCREMENT, \
+ match_id int NOT NULL, \
+ division text, \
+country text, \
+co2 int, \
+mix_1 text, \
+mix_2 text, \
+mix_3 text, \
+created_at_utc timestamp, \
+has_been_tweeted boolean, \
+tweeted_at_utc timestamp, \
+)")
