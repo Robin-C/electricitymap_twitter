eddfa7c26074a414340ae576e98fd6d39a86a9fa
Robin C <r.chesne@gmail.com>
2022-03-08 14:07:21 +0100

    fixed a bug to calculate total consumption

--- scripts/main.py
+++ scripts/main.py
@@ -58,24 +58,28 @@ def get_zones(country):
    zones_country = list(itertools.chain.from_iterable(zones_country))
    return zones_country
 
+def get_total_consumption(country):
+  zones_country = get_zones(country)
+  country_consumption_total = []
+  for zone in zones_country:
+    res = requests.get(f"https://api.electricitymap.org/v3/power-breakdown/latest?zone={zone}", headers={'auth-token':electricitymap_token}).json()
+    country_consumption_total.append(sum(res.get('powerConsumptionBreakdown').values()))
+  return country_consumption_total
+
 def get_co2(country):
    zones_country = get_zones(country)
    country_co2_levels_raw = []
    country_co2_levels_cleaned = []
-   country_production_levels = []
+   country_consumption_total = get_total_consumption(country)
 
    for zone in zones_country:
      co2 = requests.get(f"https://api.electricitymap.org/v3/carbon-intensity/latest?zone={zone}", headers={'auth-token':electricitymap_token}).json()
      country_co2_levels_raw.append(co2.get('carbonIntensity'))
-     res = requests.get(f"https://api.electricitymap.org/v3/power-breakdown/latest?zone={zone}", headers={'auth-token':electricitymap_token}).json()
-     if country_co2_levels_raw == None or res.get('error'):
-       return False
-     country_production_levels.append(res.get('powerConsumptionTotal'))
 
-   for co2_level, production_level in zip(country_co2_levels_raw, country_production_levels):
+   for co2_level, production_level in zip(country_co2_levels_raw, country_consumption_total):
       country_co2_levels_cleaned.append(co2_level*production_level)
 
-   co2 = round(sum(country_co2_levels_cleaned) / sum(country_production_levels))
+   co2 = round(sum(country_co2_levels_cleaned) / sum(country_consumption_total))
    return co2
 
 def get_mix(country):
@@ -92,16 +96,17 @@ def get_mix(country):
   'oil': 0,
   'unknown': 0,
   'hydro discharge': 0,
-  'battery discharge': 0,
-  'total_consumption': 0
+  'battery discharge': 0
   }
-
+  mix['total_consumption'] = sum(get_total_consumption(country))
+  print(country)
+  print(mix['total_consumption'])
   for zone in zones_country:
     res = requests.get(f"https://api.electricitymap.org/v3/power-breakdown/latest?zone={zone}", headers={'auth-token':electricitymap_token}).json()
     production = res.get('powerConsumptionBreakdown')
-    if production == None:
+    if production == None or mix['total_consumption'] == None:
       return False
-    mix['total_consumption'] += res.get('powerConsumptionTotal')
+    
 
    # We get mix details
     for keys in production:
@@ -129,7 +134,7 @@ def get_mix(country):
           mix['hydro discharge'] += production.get(keys) if production.get(keys) is not None else 0
         if keys == 'battery discharge':
           mix['battery discharge'] += production.get(keys) if production.get(keys) is not None else 0    
-
+  
   # We get the top 3 energy sources for the country
   tups = collections.Counter(mix).most_common(4)
   top3 = []
@@ -194,6 +199,7 @@ def get_data(division, country1, country2):
   co2_country2 = get_co2(country2)
   mix_country1 = get_mix(country1)
   mix_country2 = get_mix(country2)
+
   if co2_country1 and co2_country2 and mix_country1 and mix_country2:
     print(f'{country1}: CO2 {co2_country1}, mix {mix_country1}')
     print(f'{country2}: CO2 {co2_country2}, mix {mix_country2}')
